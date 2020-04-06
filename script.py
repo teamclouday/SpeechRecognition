140857d0b28985a9f7803d11a9b59ae45b61a21e
teamclouday <teamclouday@gmail.com>
2020-04-06 17:24:09 -0400

    update script

--- script.py
+++ script.py
@@ -7,13 +7,16 @@ data = {}
 TRAIN_DATA_PATH = os.path.join("LibriSpeech", "train-clean-360")
 TEST_DATA_PATH = os.path.join("LibriSpeech", "test-clean")
 
-def find_data(path=TRAIN_DATA_PATH):
+counter = 0
+
+def find_data(path=TRAIN_DATA_PATH, flag="Train"):
     global data
+    global counter
     handle_data = False
     for item in os.listdir(path):
         new_path = os.path.join(path, item)
         if os.path.isdir(new_path):
-            find_data(new_path)
+            find_data(new_path, flag=flag)
         else:
             handle_data = True
             break
@@ -25,25 +28,32 @@ def find_data(path=TRAIN_DATA_PATH):
                 num, text = line.rstrip().split(" ", 1)
                 filename = os.path.join(path, num+".flac")
                 y, sr = librosa.load(filename, sr=16000)
+                with open(os.path.join("dataset", flag, str(counter)+".pkl"), "wb") as outFile:
+                    pickle.dump([y, sr], outFile)
                 length = librosa.core.get_duration(y=y, sr=sr)
-                #filename = filename[3:]
+                filename = os.path.join("dataset", flag, str(counter)+".pkl")
                 data[filename] = [length, text]
                 print("{0} is loaded".format(filename))
 
-def write_data(sort=False):
+def write_data(flag="train", sort=False):
     global data
     if sort:
-        with open("data_dict_sorted.txt", "w") as outFile:
+        with open("{}_sorted.txt".format(flag), "w") as outFile:
             for key, val in sorted(data.items(), key=lambda x: x[1][0]):
                 print("{0},{1},{2}".format(key, val[0], val[1]), file=outFile)
     else:
-        with open("data_dict.txt", "w") as outFile:
+        with open("{}.txt".format(flag), "w") as outFile:
             for key, val in data.items():
                 print("{0},{1},{2}".format(key, val[0], val[1]), file=outFile)
 
 if __name__ == "__main__":
-    find_data(path=TEST_DATA_PATH)
-    # with open("temp.pickle", "wb") as outFile:
-    #     pickle.dump(data, outFile)
-    write_data()
-    # write_data(sort=True)
+    if not os.path.exists(os.path.join("dataset", "train")):
+        os.makedirs(os.path.join("dataset", "train"))
+    if not os.path.exists(os.path.join("dataset", "test")):
+        os.makedirs(os.path.join("dataset", "test"))
+    find_data(path=TRAIN_DATA_PATH, flag="train")
+    write_data(flag="train")
+    data = {}
+    counter = 0
+    find_data(path=TEST_DATA_PATH, flag="test")
+    write_data(flag="test")
